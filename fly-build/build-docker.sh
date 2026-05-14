a83c480188b4a63e312bee148f89443e7598fb63
Peter Cai <peter@fly.io>
2026-05-14 16:31:22 -0400

    Use git hash instead

--- fly-build/build-docker.sh
+++ fly-build/build-docker.sh
@@ -1,6 +1,6 @@
 PERCONA_IMG="2.6.0-ppg17.4-pgbouncer1.24.0"
 
-FLY_TAG="percona-$PERCONA_IMG-fly-$(git describe)"
+FLY_TAG="percona-$PERCONA_IMG-fly-$(git rev-parse --short HEAD)"
 FLY_IMG="ghcr.io/superfly/pgbouncer:$FLY_TAG"
 
 docker build . -f fly-build/Dockerfile --build-arg PERCONA_IMG="${PERCONA_IMG}" -t "$FLY_IMG"
