a7e954bb089a985268c96ff50e79342d6da329ca
Peter Cai <peter@fly.io>
2026-05-15 09:52:35 -0400

    Build for both percona PG 17 and 16

--- fly-build/build-docker.sh
+++ fly-build/build-docker.sh
@@ -1,8 +1,16 @@
-PERCONA_IMG="2.6.0-ppg17.4-pgbouncer1.24.0"
+#!/usr/bin/env bash
 
-FLY_TAG="percona-$PERCONA_IMG-fly-$(git rev-parse --short HEAD)"
-FLY_IMG="ghcr.io/superfly/pgbouncer:$FLY_TAG"
+build() {
+  PERCONA_IMG="2.6.0-ppg$1-pgbouncer1.24.0"
 
-docker build . -f fly-build/Dockerfile --build-arg PERCONA_IMG="${PERCONA_IMG}" -t "$FLY_IMG"
+  FLY_TAG="percona-$PERCONA_IMG-fly-$(git rev-parse --short HEAD)"
+  FLY_IMG="ghcr.io/superfly/pgbouncer:$FLY_TAG"
 
-[ "$PUSH" == "true" ] && docker push "$FLY_IMG"
+  docker build . -f fly-build/Dockerfile --build-arg PERCONA_IMG="${PERCONA_IMG}" -t "$FLY_IMG"
+
+  [ "$PUSH" == "true" ] && docker push "$FLY_IMG"
+}
+
+# Both PG 17.4 and 16.8
+build "17.4"
+build "16.8"
